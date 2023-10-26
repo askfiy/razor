2fbf07caabc66b7aeb58ba222c6fbd71f1f0fea0
askfiy <c232318108@gmail.com>
2023-10-26 10:00:36 +0800

    chore(files): add setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='http-razor',
+    version='0.4',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'aiofiles',
+        'http-router',
+        'uvicorn',
+        'uvloop',
+        'multidict',
+        'markupsafe',
+        'python-multipart',
+    ],
+    author="askfiy",
+    author_email="c2323182108@gmail.com",
+    url="https://github.com/askfiy/razor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.9"
+)
